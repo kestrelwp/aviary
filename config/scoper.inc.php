ac9a9ce17928a5a6e53025d1e2a223279a3baed8
ragulka <illimar.tambek@gmail.com>
2024-09-25 09:38:23 +0300

    Ensure project customizations are loaded, fixes #6

--- config/scoper.inc.php
+++ config/scoper.inc.php
@@ -6,13 +6,17 @@
 use Isolated\Symfony\Component\Finder\Finder;
 use Kestrel\Aviary\Patcher\DocblockPatcher;
 
+// load the base configuration
 $config = require_once __DIR__ . '/aviary.config.php';
-$project_customizations = __DIR__ . '/aviary.custom.php';
 
-if ( file_exists( $project_customizations ) ) {
+// load project customizations - this script runs inside a tmp directory of the project root,
+// so we need to go up two levels to get to the project root
+if ( $project_customizations = realpath(__DIR__ . '/../aviary.custom.php') ) {
 	require_once $project_customizations;
 }
 
+// `customize_php_scoper_config` can be defined in the project customizations file customize the config - we'll fall
+// back to a no-op function if it's not defined
 if ( ! function_exists( 'customize_php_scoper_config' ) ) {
 	function customize_php_scoper_config( array $config = [] ): array {
 		return $config;
@@ -26,6 +30,7 @@ $destination = $config['destination'];
 // remove source and destination from the config, as they're not part of php-scoper config
 unset( $config['source'], $config['destination'] );
 
+// return the configuration, piping it through any project-specific customizations
 return customize_php_scoper_config( array_merge( $config, [
 	'finders' => [
 		Finder::create()
@@ -41,6 +46,7 @@ return customize_php_scoper_config( array_merge( $config, [
 	'patchers' => [
 		new DocblockPatcher( $config ),
 	],
+	// in WordPress plugins, we don't want to expose global constants, classes, or functions
 	'expose-global-constants' => false,
 	'expose-global-classes'   => false,
 	'expose-global-functions' => false,
